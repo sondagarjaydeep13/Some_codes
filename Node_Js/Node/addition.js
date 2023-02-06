f8f2ca7a7ab65428402c42729a646ce839a20b02
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-02-06 13:31:27 +0530

    Node Js

---
+++ Node_Js/Node/addition.js
@@ -0,0 +1,6 @@
+function add(){
+    var a=document.getElementById('a').value
+    var b=document.getElementById('b').value
+    var resultab=Number(a)+Number(b);
+    document.getElementById('resultab').innerHTML=resultab;
+}
