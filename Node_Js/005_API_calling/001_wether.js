8193ea354caa5225490ca2d03daf866169fdbd02
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-02-11 11:32:50 +0530

    Node Js

---
+++ Node_Js/005_API_calling/001_wether.js
@@ -0,0 +1,7 @@
+const axios=require("axios");
+const url="https://api.openweathermap.org/data/2.5/weather?lat=21.170240&lon=72.831062&appid=dee1453ce9972e4b53e8058399a68e05"
+
+axios.get(url).then(result=>{
+   console.log(result.data.main.temp-273);
+   console.log(result.data.name);
+})
