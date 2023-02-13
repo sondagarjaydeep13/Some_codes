89a2090b332ec9666234ccce1b9267debb7faadd
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-02-13 18:44:01 +0530

    Node Js

---
+++ Node_Js/005_API_calling/covid-19.js
@@ -0,0 +1,15 @@
+const axios=require("axios");
+
+const url="https://data.covid19india.org/v4/min/data.min.json";
+
+axios.get(url).then(result=>{
+   console.log(result.data.GJ.districts.Amreli);
+  
+    // for(var i=0;i<=result.data.length;i++){
+    //        console.log(result.data[i]);
+       //    }
+    // console.log(result[0]);
+    
+}).catch(err=>{
+        console.log(err);
+})
