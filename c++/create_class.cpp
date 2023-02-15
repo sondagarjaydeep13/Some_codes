dbb8678c6e0a52e7447af8346aac6ed9fa2ca55f
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-02-15 18:56:56 +0530

    c++

--- c++/create_class.cpp
+++ c++/create_class.cpp
@@ -1,22 +1,25 @@
 #include<iostream>
 using namespace std;
 
-class student{
-    int number,per;
-    string name;
+class twosqure{
+    int a,b,squre;
     public:
-    void studentData(int a,string b,int c){
-        this->number=a;
-        this->name=b;
-        this->per=c;
-
-        cout<<"Student number is ="<<a<<endl;
-        cout<<"Student name is ="<<b<<endl;
-        cout<<"Student per is ="<<c<<endl;
+    void abvalue(){
+        cout<<"Enter value of a:";
+        cin>>a;
+        cout<<"Enter value of b:";
+        cin>>b;
+    }
+    void findsqure(){
+          squre=(a+b)*(a+b);
+          cout<<squre;
     }
 };
-int main(){
-    student s1;
-    s1.studentData(1,"Raj Kumar",75);
+int main()
+{
+    twosqure t1;
+    t1.abvalue();
+    t1.findsqure();
+    
     return 0;
 }
