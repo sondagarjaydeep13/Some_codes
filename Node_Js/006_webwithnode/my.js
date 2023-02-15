f569be0c53464b1a61cf2ece522f513f0055df7c
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-02-15 10:05:25 +0530

    Node Js

--- Node_Js/006_webwithnode/my.js
+++ Node_Js/006_webwithnode/my.js
@@ -8,6 +8,9 @@ const path=require("path");
 webpage.get("/web",(req,resp)=>{
     resp.sendFile(path.join(__dirname,"my.html"));
 })
+webpage.get("/google",(req,resp)=>{
+    resp.sendFile(path.join(__dirname,"google.html"));
+})
 
 webpage.listen(port,()=>{
     console.log("port running..port number:"+port);
