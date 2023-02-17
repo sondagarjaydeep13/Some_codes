fb4747927486c72c188f2a5c29e0cb584b83baf4
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-02-17 22:12:44 +0530

    Html & Node Js

--- Html_Css/registration-form/registration.js
+++ Html_Css/registration-form/registration.js
@@ -1,4 +1,5 @@
 const express=require("express");
+const { join } = require("path");
 const app=express();
 const port=9600;
 
@@ -7,9 +8,11 @@ const path=require("path");
 app.listen(port,()=>{
     console.log(`port number running ${port} ..!!`);
 })
-
+app.get("/header",(req,resp)=>{
+    resp.sendFile(path.join(__dirname,"Header.html"));
+})
 app.get("/registration",(req,resp)=>{
-    resp.sendFile(path.join(__dirname,"Registration_form.html"));
+    resp.sendFile(path.join(__dirname,"registration.html"));
 })
 app.get("/submit",(req,resp)=>{
     resp.sendFile(path.join(__dirname,"reg_submit.html"));
@@ -18,9 +21,15 @@ app.get("/signin",(req,resp)=>{
     resp.sendFile(path.join(__dirname,"signin_form.html"))
 })
 app.get("/login",(req,resp)=>{
-    resp.sendFile(path.join(__dirname,"login_success.html"));
+    resp.sendFile(path.join(__dirname,"signin_form.html"));
 
 })
 app.get("/logout",(req,resp)=>{
     resp.sendFile(path.join(__dirname,"signin_form.html"));
+})
+
+// for forgot passsword
+
+app.get("/forgot",(req,resp)=>{
+    resp.sendFile(path.join(__dirname,"forgot_password.html"))
 })
