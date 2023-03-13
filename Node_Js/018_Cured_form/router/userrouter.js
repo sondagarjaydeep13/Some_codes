3c3606aa893482acb9fb93d83ad33ad700434cf9
sondagarjaydeep13 <113520283+sondagarjaydeep13@users.noreply.github.com>
2023-03-13 16:30:05 +0530

    Node Js

--- Node_Js/018_Cured_form/router/userrouter.js
+++ Node_Js/018_Cured_form/router/userrouter.js
@@ -5,18 +5,23 @@ router.get("/", (req, resp) => {
   resp.render("index");
 });
 router.post("/adduser", async (req, resp) => {
-  const user = new User({
-    uname: req.body.uname,
-    email: req.body.email,
-    pass: req.body.pass,
-    pnumber: req.body.pnumber,
-  });
+  var id = req.body._id;
+  try {
+    if (id == "") {
+      const user = new User({
+        uname: req.body.uname,
+        email: req.body.email,
+        pass: req.body.pass,
+        pnumber: req.body.pnumber,
+      });
 
-  const data = await user.save();
-  if (data) {
+      await user.save();
+    } else {
+      await User.findByIdAndUpdate(id, req.body);
+    }
     resp.render("success");
-  } else {
-    resp.send("Invalide Data");
+  } catch (error) {
+    console.log(error);
   }
 });
 router.get("/backhome", (req, resp) => {
@@ -26,5 +31,27 @@ router.get("/infodata", async (req, resp) => {
   const userdata = await User.find();
 
   resp.render("infodata", { udata: userdata });
+});
+
+router.get("/edit", async (req, resp) => {
+  const _id = req.query.editid;
+
+  try {
+    const data = await User.findOne({ _id: _id });
+
+    resp.render("index", { edata: data });
+  } catch (error) {
+    resp.send(error);
+  }
+});
+router.get("/delete", async (req, resp) => {
+  const _id = req.query.deleteid;
+
+  try {
+    await User.findByIdAndDelete(_id);
+    resp.redirect("infodata");
+  } catch (error) {
+    resp.send(error);
+  }
 });
 module.exports = router;
